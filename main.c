0d4cfd3fde966781d84a3984c73c09c651d92a08
Hyerim Kim <hyerkim@c6r4s4.42seoul.kr>
2021-03-24 21:24:15 +0900

    modified

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 char *check_sigle(char *s, char c)
 {
     int i;
+	int j;
     char *new;
 
     i = 0;
+	j = 0;
     new
     while (s[i])
     {
-        if (s[i] )
+        if (s[i] != c)
+		{
+			new[j] = s[i];
+			j++;
+		}
+		i++;
     }
+	new[j] = '\0';
+	return (new);
 }
